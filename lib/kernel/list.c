24a3eb00223e124199f52bf3fa90260f55cf3154
junp <jpchen78@foxmail.com>
2021-07-01 23:05:13 +0800

    implement double linked list

---
+++ lib/kernel/list.c
@@ -0,0 +1,85 @@
+#include "list.h"
+#include "interrupt.h"
+#include "stdbool.h"
+#include "string.h"
+
+void list_init(struct list*) {
+  list->head.next = &list->tail;
+  list->head.prev = NULL;
+  list->tail.prev = &list->head;
+  list->tail.next = NULL;
+}
+
+void list_insert_before(struct list_elem* before, struct list_elem* elem) {
+  enum intr_status old_status = intr_disable();
+
+  elem->prev = before->prev;
+  elem->next = before;
+  before->prev->next = elem;
+  before->prev = elem;
+
+  intr_set_status(old_status);
+}
+
+void list_push(struct list* plist, struct list_elem* elem) {
+  list_insert_before(plist->head.next, elem);
+}
+
+void list_iterate(struct list* plist) {
+  
+}
+
+void list_append(struct list_* plist, struct list_elem* elem) {
+  list_insert_before(&plist->tail, elem);
+}
+
+void list_remove(struct list_elem* elem) {
+  enum intr_status old_status = intr_disable();
+
+  elem->prev->next = elem->next;
+  elem->next->prev = elem->prev;
+
+  intr_set_status(old_status);
+}
+
+struct list_elem* list_pop(struct list_elem* plist) {
+  struct list_elem* pelem = plist->head.next;
+  list_remove(pelem);
+  return pelem;
+}
+
+uint32_t list_len(struct list* plist) {
+  struct list_elem* elem = plist->head.next;
+  uint32_t i = 0;
+  while (elem != &plist->tail) {
+    i++;
+    elem = elem->next;
+  }
+  return i
+}
+
+struct list_elem* list_tranversal(struct list* plist, function func, int arg) {
+  struct list_elem* elem = plist->head.next;
+  while (elem != &plist->tail) {
+    if (func(elem, arg)) {
+      return elem;
+    }
+    elem = elem->next;
+  }
+  return NULL;
+}
+
+bool elem_find(struct list* plist, struct list_elem* obj_elem) {
+  struct list_elem* elem = plist->head.next;
+  while (elem != &plist->tail) {
+    if (elem == obj_elem) {
+      return true;
+    }
+    elem = elem->next;
+  }
+  return false;
+}
+
+bool list_empty(struct list_elem* plist) {
+  return (plist->head.next == &plist->tail) ? true : false;
+}
---
+++ lib/kernel/list.h
@@ -0,0 +1,33 @@
+#ifndef __KERNEL_LIST_H
+#define __KERNEL_LIST_H
+#include "stdint.h"
+
+/* offset of a member in struct type */
+#define offset(struct_type, member) ((int)(&((struct_type*)0)->member))
+/* given a member ptr in a struct, return the struct ptr */
+#define elem2entry(struct_type, member, elem_ptr) \
+          ((struct_type*)((int)elem_ptr - offset(struct_type, member)))
+
+struct list_elem {
+  struct list_elem* prev;
+  struct list_elem* next;
+};
+
+struct list {
+  struct list_elem head;
+  struct list_elem tail;
+};
+
+typedef bool (function) (struct list_elem*, int arg);
+
+void list_init(struct list*);
+void list_insert_before(struct list_elem* before, struct list_elem* elem);
+void list_push(struct list* plist, struct list_elem* elem);
+void list_append(struct list_* plist, struct list_elem* elem);
+void list_remove(struct list_elem* elem);
+struct list_elem* list_pop(struct list_elem* plist);
+uint32_t list_len(struct list* plist);
+struct list_elem* list_tranversal(struct list* plist, function func, int arg);
+bool elem_find(struct list* plist, struct list_elem* obj_elem);
+bool list_empty(struct list* plist);
+#endif
