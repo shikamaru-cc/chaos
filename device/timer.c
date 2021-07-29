2210d9b325a7b2b3a19880e390eac35e52caf33f
junp <jpchen78@foxmail.com>
2021-07-29 18:38:13 +0800

    add sys_sleep

--- device/timer.c
+++ device/timer.c
@@ -1,12 +1,13 @@
 #include "debug.h"
+#include "global.h"
 #include "stdint.h"
 #include "timer.h"
 #include "thread.h"
 #include "kernel/io.h"
 #include "kernel/print.h"
 
-#define IRQ0_FREQUENCY    100
-#define INPUT_FREQUENCY   1193180 /* timer device CLK frequency */
+#define IRQ0_FREQUENCY    100      // 100 timer interrupt per second
+#define INPUT_FREQUENCY   1193180  // timer device CLK frequency
 #define PIT_CONTROL_PORT  0x43
 #define COUNTER0_VALUE    INPUT_FREQUENCY / IRQ0_FREQUENCY
 #define COUNTER0_PORT     0x40
@@ -42,6 +43,28 @@ static void timer_set_frequence(uint8_t counter_port,
   outb(counter_port, (uint8_t)counter_value);
   /* write high 8 bit */
   outb(counter_port, (uint8_t)(counter_value >> 8));
+}
+
+void ticksleep(uint32_t sleep_ticks) {
+  uint32_t start_tick = ticks;
+  while (1) {
+    if (ticks - start_tick < sleep_ticks) {
+      thread_yield();
+    }
+  }
+}
+
+void sys_milisleep(uint32_t miliseconds) {
+  uint32_t milisecond_per_intr = 1000 / IRQ0_FREQUENCY;
+  uint32_t sleep_ticks = DIV_ROUND_UP(miliseconds, milisecond_per_intr);
+  ASSERT(sleep_ticks > 0);
+  ticksleep(sleep_ticks);
+}
+
+void sys_sleep(uint32_t seconds) {
+  uint32_t sleep_ticks = seconds * IRQ0_FREQUENCY;
+  ASSERT(sleep_ticks > 0);
+  ticksleep(sleep_ticks);
 }
 
 void timer_init() {
--- device/timer.h
+++ device/timer.h
@@ -1,5 +1,7 @@
 #ifndef __DEVICE_TIMER_H
 #define __DEVICE_TIMER_H
 #include "stdint.h"
+void sys_milisleep(uint32_t miliseconds);
+void sys_sleep(uint32_t seconds);
 void timer_init(void);
 #endif
